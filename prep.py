17d4e79346b8c2f34f890ec77ceaf213cc49974b
SARVESHKHANDELWAL <sarveshk21122001@gmail.com>
2023-07-13 19:24:39 +0530

    Implementation run on 13/07/23

---
+++ prep.py
@@ -0,0 +1,91 @@
+# python program to illustrate If else statement
+#!/usr/bin/python
+# from math import sqrt 
+
+# i = 20
+# if (i < 15):
+# 	print("i is smaller than 15")
+# 	print("i'm in if Block")
+# else:
+# 	print("i is greater than 15")
+# 	print("i'm in else Block")
+# print(sqrt(4))
+
+
+
+# # math functions
+
+# def func(a: int, b: int):
+#     return a*b
+
+# print(func(4,5))
+
+# While Loop 
+
+# i=1
+# while i<5:
+#     print(i)
+#     i+=1
+
+
+# Name logic printing
+
+# len =2
+
+# while True:
+#     name = input("please enter the name")
+#     if len(name)>= len and name.isprintbale() and name.isalpha():
+#         break
+# print("hello {0}".format(name))
+
+
+# value is not there so we can append that into array
+
+# l = [1,2,3]
+# val = 10
+
+# found  = False
+# idx =0
+# while idx < len(l):
+#     if l[idx] == val:
+#         found = true
+#         break
+#     idx+=1
+
+
+# if not found:
+#    l.append(val)
+
+# print(l)
+
+
+# try and except 
+
+
+# def divide(x, y):
+# 	try:
+# 		result = x // y
+# 		print("Your answer is :", result)
+# 	except ZeroDivisionError:
+# 		print("Sorry ! You are dividing by zero ")
+
+
+# divide(3, 2)
+# divide(3, 0)
+
+
+
+for c in 'hello':
+    print(c)
+
+
+
+
+
+
+
+
+
+
+
+
